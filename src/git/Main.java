d349c239ed4053ebf7a60c25942f95b5b1e28a22
Sandra Escandell <sandra.escandell.montes@gmail.com>
2019-11-18 20:18:58 +0100

    Commit 3: copiado código del ejercicio

--- src/git/Main.java
+++ src/git/Main.java
@@ -1,10 +1,11 @@
 package git;
 
 public class Main {
-
-	public static void main(String[] args) {
-	
-		System.out.println("Hola món");
-	}
-
+public static void main(String[] args) {
+printHola();
 }
+public static void printHola(){
+System.out.println("Hola món!");
+}
+}
+
